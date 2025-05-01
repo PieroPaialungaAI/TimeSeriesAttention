1a103266aa750d6c0c191544c7b43403ed801fa7
Piero Paialunga <piero.paialunga@hotmail.com>
2025-04-30 21:25:27 -0400

    Working on modified sine function

--- data.py
+++ data.py
@@ -2,3 +2,11 @@ import numpy as np
 
 def build_sine_wave(X, frequency, amplitude):
     return amplitude * np.sin(X/frequency)
+
+
+def build_modified_sine_wave(X, frequency, amplitude):
+    return modify_sine(build_sine_wave(X,frequency, amplitude))
+
+
+def modify_sine(sine_wave):
+    pass

245be1bda50b8a3ac358fc39cab97787674d6679
Piero Paialunga <piero.paialunga@hotmail.com>
2025-05-08 09:30:21 -0400

    Implemented the modification function

--- data.py
+++ data.py
@@ -4,9 +4,11 @@ def build_sine_wave(X, frequency, amplitude):
     return amplitude * np.sin(X/frequency)
 
 
-def build_modified_sine_wave(X, frequency, amplitude):
-    return modify_sine(build_sine_wave(X,frequency, amplitude))
+def build_modified_sine_wave(X, frequency, amplitude, loc, length):
+    return modify_sine(build_sine_wave(X,frequency, amplitude), loc, length)
 
 
-def modify_sine(sine_wave):
-    
+def modify_sine(sine_wave, loc, length):
+    x_value = sine_wave[loc]
+    sine_wave[loc:loc+length] = x_value
+    return x_value
